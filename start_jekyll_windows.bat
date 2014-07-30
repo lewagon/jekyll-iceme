57bf9c8bc006c1cb8d0852542f1c9198a4fe86c5
Sebastien Saunier <seb@saunier.me>
2014-07-30 18:24:01 +0200

    Boilerplate Jekyll

---
+++ start_jekyll_windows.bat
@@ -0,0 +1,8 @@
+@echo off
+echo. & echo. & echo. &echo.
+echo -------------------------------------------------
+echo Open your browser and go to http://localhost:4000
+echo -------------------------------------------------
+echo. & echo. & echo. &echo.
+
+jekyll serve --watch --baseurl=""
